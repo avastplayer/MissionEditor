7541efd0b03f4fc8530f50d22c72a82e83e3411f
嘉诚 卢 <lujiacheng45@hotmail.com>
2017-05-25 21:23:07 +0800

    调整布局

--- MissionEditor/MainWindow.xaml.cs
+++ MissionEditor/MainWindow.xaml.cs
@@ -6,6 +6,7 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Input;
 using System.Windows.Media.Imaging;
+using Xceed.Wpf.AvalonDock.Themes.Office2007;
 
 namespace MissionEditor
 {
